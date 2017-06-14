4187f5de1fce8bd405e6358fbad2998773dc39a6
Amiry <amirygames@gmail.com>
2017-06-14 12:56:27 +0300

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+render: function () {
+	return (
+	<div>
+	 <h1> ~ Hello World!</h1>
+	 <h2> This is my fresh new React App </h2>
+	</div>
+	)
+}
+});
+
+
+module.exports = App;
